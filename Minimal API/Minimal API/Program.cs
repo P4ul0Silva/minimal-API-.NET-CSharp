75c486204406bed5ec0be02c6589cbe5834b6e7b
P4ul0Silva <paulors43@outlook.com>
2023-10-04 13:50:55 -0300

    configuração mínima para criação de rota GET com retorno 200 OK de objetos classe Movies.

--- Minimal API/Minimal API/Program.cs
+++ Minimal API/Minimal API/Program.cs
@@ -25,24 +25,11 @@ List<Movie> movies = new()
     new() {Id = 5, Rating = 5, Title = "The Matrix" }
 };
 
-var summaries = new[]
+app.MapGet("/api/Movies", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    return Results.Ok(movies);
+});
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
 
